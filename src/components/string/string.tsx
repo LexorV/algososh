a626010ceeacdd67b2efceb1ec37d1da9fbcd9cf
Lex <Lexor.V.A.S@yandex.ru>
2022-05-31 19:35:36 +0300

    add string component

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import stringStyle from './string.module.css'
+
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
+      <div className={stringStyle.box_main}>
+      <div className={stringStyle.box_input}>
+      <Input type="text" isLimitText={true} maxLength={11} />
+      <Button text='Развернуть' />
+      </div>
+      </div>
      
     </SolutionLayout>
   );
