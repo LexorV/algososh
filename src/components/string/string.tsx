6c200749c43034588ed50b171d365ffb1b3f6a74
Lex <Lexor.V.A.S@yandex.ru>
2022-06-03 23:38:39 +0300

    fix logics string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/button/button";
 import stringStyle from './string.module.css';
 import { Circle } from '../ui/circle/circle'
 import { ElementStates } from '../../types/element-states';
+import { start } from "repl";
 type TobjectText = {
   id: number,
   text: string,
@@ -20,59 +21,50 @@ export const StringComponent: React.FC = () => {
     setTextInput(e.target.value);
   }
   const uid = () => Date.now().toString(36) + Math.random().toString(36);
-/*
-  const sortArr = (arr: string[]) => {
-    for (let n = 0; n < arr.length; n++) {
-      for (let i = 0; i <= arr.length - 1 - n; i++) {
-        if (arr[i] < arr[i + 1]) {
-          let el1 = arr[i]
-          let el2 = arr[i + 1]
-          arr[i] = el2;
-          arr[i + 1] = el1;
-        }
-      }
-    }
-  }*/
-
-  const bubbleSort = async (
+  const stringSort = async (
     arrText: Array<TobjectText>,
     setLeft: (n: number) => void,
     setRight: (n: number) => void,
     setArrText: (arrText: Array<TobjectText>) => void,
   ) => {
     const copy = [...arrText];
-    console.log(copy)
-    
-    for (let i = 0; i < copy.length; i++) {
-      for (let j = 0; j < copy.length - 1 - i; j++) {
-        const right = copy[j + 1]
-        const left = copy[j]
-        
+    let low = 0;
+    let hight = copy.length - 1;
+    const left = copy[low]
+    const right = copy[hight]
+    if (!copy) {
+      return;
+    }
+    while (low <= hight) {
+      copy[low].style = ElementStates.Changing
+      copy[hight].style = ElementStates.Changing
+      if(left.text <= right.text) {
         await new Promise<void>((res) => {
           setTimeout(() => {
             res()
           }, 1000)
         })
-        setLeft(j)
-        setRight(j + 1)
-        right.style = ElementStates.Modified
-        
-        if (left.text > right.text) {
-          right.style = ElementStates.Changing
-          const x = copy[j]
-          copy[j] = copy[j + 1]
-          copy[j + 1] = x
-        }
-        if(copy.length<=1) {
-          left.style = ElementStates.Modified
-        }
+        setLeft(low)
+        setRight(hight)
+        copy[low].style = ElementStates.Modified
+        copy[hight].style = ElementStates.Modified
+        const x = copy[low]
+        copy[low] = copy[hight]
+        copy[hight] = x
+        low++;
+        hight--;
         setArrText(copy)
       }
+      else {
+        low++;
+        hight--;
+      }
     }
+    setArrText(copy)
   }
   const startAlgo = async () => {
     setStarted(true)
-    await bubbleSort(arrText, setLeft, setRight, setArrText)
+    await stringSort(arrText, setLeft, setRight, setArrText)
     setStarted(false)
     setLeft(null)
     setRight(null)
@@ -90,29 +82,11 @@ export const StringComponent: React.FC = () => {
     })
     setArrText(arrObjText);
   }
- 
   useEffect(() => {
-    console.log(started)
-
     if(arrText.length > 1 && started == false) {
-     
       startAlgo()
-      /*
-        let timer =  setTimeout(() => {
-          sortArr(arrText)
-          setArrText([...arrText])
-        }, 1000);
-        return () => {
-          clearTimeout(timer);
-        };*/
       }
     },[arrText])
-  /*
-  useEffect(() => {
-      clearTimeout(timerRef.current as NodeJS.Timeout)
-  }, []);*/
- // console.log(arrText);
-
   return (
     <SolutionLayout title="Строка">
       <div className={stringStyle.box_main}>
@@ -124,11 +98,10 @@ export const StringComponent: React.FC = () => {
           <Button isLoader={started} type="submit" text='Развернуть' />
         </form>
         <ul className={stringStyle.lists_circle}>
-
-          {arrText.map((list, index) => { 
+          {arrText.map((list) => {
           return  (
           <li key={uid()} >
-            <Circle index={index} state={list.style} letter={list.text} />
+            <Circle state={list.style} letter={list.text} />
           </li>)})}
         </ul>
       </div>
