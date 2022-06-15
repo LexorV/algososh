28b263295f2e419b9f1e6d42c7dc7c9042947b64
Lex <Lexor.V.A.S@yandex.ru>
2022-06-15 05:37:48 +0300

    fix list-page

--- src/components/list-page/linkedList.ts
+++ src/components/list-page/linkedList.ts
@@ -14,10 +14,8 @@ export interface ILinkedList {
     getSize: () => number;
     toArray: () => void;
     prepend: (element: TobjectText) => void
-    addByIndex: (element:TobjectText, index: number, al:ReactElement) => void;
-    deleteByIndex: (
-        index:number,
-        cicleCallback: (text:string | number) => ReactElement) => void;
+    addByIndex: (element:TobjectText, index: number) => void;
+    deleteByIndex: (index:number,) => void;
     deleteHead: () => void;
     deleteTail: () => void;
 }
@@ -26,106 +24,41 @@ export class LinkedList<T> implements ILinkedList {
     private size: number;
     private tail: Node<TobjectText> | null;
     arr: Node<TobjectText>[];
-    setArr: React.Dispatch<React.SetStateAction<Node<TobjectText>[]>>;
-    setChange: React.Dispatch<React.SetStateAction<boolean>>;
-    setStarted: React.Dispatch<React.SetStateAction<boolean>>;
     constructor(
-        arr: Node<TobjectText>[],
-        setArr: React.Dispatch<React.SetStateAction<Node<TobjectText>[]>>,
-        setChange: React.Dispatch<React.SetStateAction<boolean>>,
-        setStarted: React.Dispatch<React.SetStateAction<boolean>>) 
+        arr: Node<TobjectText>[],)
         {
         this.arr = arr;
         this.head = null;
         this.size = 0;
         this.tail = null;
-        this.setArr = setArr;
-        this.setChange = setChange;
-        this.setStarted = setStarted;
     }
 
-    addByIndex = async (element: TobjectText, index: number, al: ReactElement) => {
+    addByIndex = async (element: TobjectText, index: number) => {
         if (index < 0 || index > this.size) {
             console.log('Enter a valid index');
             return;
         } else {
             const node = new Node(element);
             if (index === 0 && this.head) {
-                this.setChange(true)
-                console.log(this.head)
-                this.head.value && (this.head.value.head = al)
-                await new Promise<void>((res) => {
-                    setTimeout(() => {
-                        res()
-                    }, 1000)
-                });
-                this.setChange(false)
                 node.next = this.head;
-                node.value.head = 'head'
-                this.head.value.head = ''
                 this.head = node;
-                this.setArr(this.toArray());
-                await new Promise<void>((res) => {
-                    setTimeout(() => {
-                        res()
-                    }, 1000)
-                });
-                node.value.style = ElementStates.Default
             } else {
                 let curr = this.head;
                 let currIndex = 0;
                 let prev = null;
-                if(curr) {
-                    curr.value.style = ElementStates.Changing
-                    curr.value.head = al;
-                }
                 while (currIndex < index) {
-                    this.setChange(true)
-                    currIndex++
-                    await new Promise<void>((res) => {
-                        setTimeout(() => {
-                            res()
-                        }, 1000)
-                    });
                     prev = curr;
-                    if(prev) {
-                    prev.value.style = ElementStates.Changing;
-                    currIndex === 1 ? prev.value.head = "head" : prev.value.head = '';
                     curr && (curr = curr.next);
-                    curr && (curr.value.head = al);
-                    this.setChange(false)
-                    if (currIndex == index && curr) {
-                        prev.value.head === "head" ? prev.value.head = 'head' : prev.value.head = '';
-                        curr.value.head = '';
-                        this.defaultColor();
-                    }
-
-                    }
-                    else {
-                        if(curr) {
-                        curr.value.style = ElementStates.Changing
-                        curr.value.head = al;
-                        }
-                    }
+                    currIndex++
                 }
-                await new Promise<void>((res) => {
-                    setTimeout(() => {
-                        res()
-                    }, 1000)
-                });
                 node.next = curr;
                 prev && (prev.next = node);
-                this.setArr(this.toArray())
-                await new Promise<void>((res) => {
-                    setTimeout(() => {
-                        res()
-                    }, 1000)
-                });
-                prev && prev.next && (prev.next.value.style = ElementStates.Default)
             }
-            this.size++;
-        }
+        this.size++;
+}
     }
+
+    
     defaultColor() {
         let currentNode = this.head;
         while (currentNode) {
@@ -133,7 +66,7 @@ export class LinkedList<T> implements ILinkedList {
             currentNode = currentNode.next;
         }
     }
-    deleteByIndex = async (index: number, cicleCallback: (text:string | number) => ReactElement) => {
+    deleteByIndex = async (index: number) => {
         if (index >= this.size) {
             throw new Error("error size");
         }
@@ -143,53 +76,15 @@ export class LinkedList<T> implements ILinkedList {
         let prev = null;
         let curr = this.head;
         let currentIndex = 0;
-        curr && (curr.value.style = ElementStates.Changing)
         while (currentIndex < index) {
-            this.setChange(true)
             currentIndex++
-            await new Promise<void>((res) => {
-                setTimeout(() => {
-                    res()
-                }, 1000)
-            });
             prev = curr;
             curr && (curr = curr.next);
-            this.setChange(false)
-
-            if (currentIndex == index && curr) {
-                this.setChange(true)
-                let text = curr.value.text
-                curr.value.style = ElementStates.Changing;
-                this.setArr(this.toArray());
-                await new Promise<void>((res) => {
-                    setTimeout(() => {
-                        res()
-                    }, 1000)
-                });
-                curr.value.text = '';
-                curr.value.style = ElementStates.Default;
-                curr.value.tail = cicleCallback(text);
-                this.setChange(false);
-            }
-            else {
-                curr && (curr.value.style = ElementStates.Changing)
-
-
-            }
-
         }
-        await new Promise<void>((res) => {
-            setTimeout(() => {
-                res()
-            }, 1000)
-        });
         if(prev && curr) {
-            curr.next == null && (prev.value.tail = 'tail');
+            curr.next == null;
             prev.next = curr.next;
         }
-        this.defaultColor();
-        this.setArr(this.toArray());
-
         this.size--;
         return this;
     }
--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent,useEffect, useRef } from "react";
+import React, { useState, ChangeEvent, useEffect, useRef } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
@@ -6,10 +6,10 @@ import { Circle } from '../ui/circle/circle';
 import { nanoid } from "nanoid";
 import { ElementStates } from '../../types/element-states';
 import listStyle from './list-page.module.css';
-import {ArrowIcon} from '../ui/icons/arrow-icon';
-import  { TobjectText } from '../../types';
-import {LinkedList } from './linkedList';
-import {Node} from './linkedList';
+import { ArrowIcon } from '../ui/icons/arrow-icon';
+import { TobjectText } from '../../types';
+import { LinkedList } from './linkedList';
+import { Node } from './linkedList';
 export const ListPage: React.FC = () => {
   const [started, setStarted] = useState<boolean>(false);
   const [textInput, setTextInput] = useState<string>('');
@@ -23,64 +23,64 @@ export const ListPage: React.FC = () => {
   const [btnAddByIndex, setBtnAddByIndex] = useState<boolean>(false);
   const [btnDellByIndex, setDellAddByIndex] = useState<boolean>(false);
 
-  //any т.к. иначе везде требуется делать проверку на null
+  //any т.к. иначе во всех объектах требуется делать проверку на null
   const ref = useRef<any>(null);
   function LinkedListAdd() {
     if (ref.current === null) {
-      ref.current = new LinkedList<TobjectText>(listArray, setListArray, setChange, setStarted);
+      ref.current = new LinkedList<TobjectText>(listArray);
     }
     return ref.current;
   }
-  const linkedList =  LinkedListAdd();
+  const linkedList = LinkedListAdd();
   const generatArray = () => {
     for (let i = 0; i < 4; i++) {
       linkedList.append({
         text: String(Math.floor(Math.random() * (99 - 1)) + 1),
         style: ElementStates.Default,
-        head: i == 3 ? 'head':'',
-        tail: i == 0 ? 'tail':''
+        head: i == 3 ? 'head' : '',
+        tail: i == 0 ? 'tail' : ''
       })
     }
     setListArray(linkedList.toArray())
   }
   useEffect(() => {
     generatArray()
-  }, [])
- const onFormTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+  }, []);
+  const onFormTextChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTextInput(e.target.value);
   }
   const onFormindexChange = (e: ChangeEvent<HTMLInputElement>) => {
     setIndexInput(parseInt(e.target.value));
   }
-const headCircleChange =  async() => {
-  linkedList.head.value.head = <Circle
-  letter={textInput}
-  state={ElementStates.Changing}
-  isSmall={true}
-/>
-}
-const headColorChange = async() => {
-  linkedList.head.value.head = 'head';
-  linkedList.head.next.value.head = '';
-  setChange(true)
-  linkedList.head.value.style = ElementStates.Modified
-  await new Promise<void>((res) => {
-    setTimeout(() => {
+  const headCircleChange = async () => {
+    linkedList.head.value.head = <Circle
+      letter={textInput}
+      state={ElementStates.Changing}
+      isSmall={true}
+    />
+  }
+  const headColorChange = async () => {
+    linkedList.head.value.head = 'head';
+    linkedList.head.next.value.head = '';
+    setChange(true)
+    linkedList.head.value.style = ElementStates.Modified
+    await new Promise<void>((res) => {
+      setTimeout(() => {
         res()
-    }, 1000)
-});
-linkedList.head.value.style = ElementStates.Default
-setChange(false)
-}
-  const addElHead = async() => {
+      }, 1000)
+    });
+    linkedList.head.value.style = ElementStates.Default
+    setChange(false)
+  }
+  const addElHead = async () => {
     setStarted(true)
     setbtnAddHead(true)
     headCircleChange()
     await new Promise<void>((res) => {
       setTimeout(() => {
-          res()
+        res()
       }, 1000)
-  });
+    });
     linkedList.append({
       text: textInput,
       style: ElementStates.Default,
@@ -93,43 +93,43 @@ setChange(false)
     setTextInput('')
     setbtnAddHead(false)
   }
-  const tailColorChange = async() => {
+  const tailColorChange = async () => {
     linkedList.tail.value.tail = '';
     setChange(true)
     linkedList.tail.value.style = ElementStates.Modified
     await new Promise<void>((res) => {
       setTimeout(() => {
-          res()
+        res()
       }, 1000)
-  });
-  linkedList.tail.value.style = ElementStates.Default;
-  linkedList.tail.value.tail = 'tail'
-setChange(false)
-}
-  const tailCircleChange =  async() => {
+    });
+    linkedList.tail.value.style = ElementStates.Default;
+    linkedList.tail.value.tail = 'tail'
+    setChange(false)
+  }
+  const tailCircleChange = async () => {
     setChange(true)
     linkedList.tail.value.tail = <Circle
-    letter={textInput}
-    state={ElementStates.Changing}
-    isSmall={true}
-  />
-  await new Promise<void>((res) => {
-    setTimeout(() => {
+      letter={textInput}
+      state={ElementStates.Changing}
+      isSmall={true}
+    />
+    await new Promise<void>((res) => {
+      setTimeout(() => {
         res()
-    }, 1000)
-});
-linkedList.tail.value.tail = ''
-setChange(false)
+      }, 1000)
+    });
+    linkedList.tail.value.tail = ''
+    setChange(false)
   }
-  const addElTail = async() => {
+  const addElTail = async () => {
     setbtnAddTail(true)
     setStarted(true)
     tailCircleChange()
     await new Promise<void>((res) => {
       setTimeout(() => {
-          res()
+        res()
       }, 1000)
-  });
+    });
     linkedList.prepend({
       text: textInput,
       style: ElementStates.Modified,
@@ -142,55 +142,63 @@ setChange(false)
     setTextInput('')
     setbtnAddTail(false)
   }
-  const animDeleteHead = async() => {
+  const animDeleteHead = async () => {
     setChange(true)
-    linkedList.head.next.value.head = 'head';
+ 
     let text = linkedList.head.value.text
     linkedList.head.value.head = <Circle
-    letter={text}
-    state={ElementStates.Changing}
-    isSmall={true}
-  />
-  linkedList.head.value.text = '';
-  setChange(false)
+      letter={text}
+      state={ElementStates.Changing}
+      isSmall={true}
+    />
+    linkedList.head.value.text = '';
+    await new Promise<void>((res) => {
+      setTimeout(() => {
+        res()
+      }, 1000)
+    });
+
+    linkedList.head.next.value.head = 'head';
+    linkedList.head.value.text = '';
+    setChange(false)
 
   }
-  const deleteHead = async() => {
+  const deleteHead = async () => {
     setBtnDellHead(true)
     setStarted(true)
     animDeleteHead()
     await new Promise<void>((res) => {
       setTimeout(() => {
-          res()
+        res()
       }, 1000)
-  });
+    });
     linkedList.deleteHead()
     setListArray(linkedList.toArray())
     setStarted(false)
     setBtnDellHead(false)
 
   }
-const animDeleteTail = async() => {
-  setChange(true)
-  let text = linkedList.tail.value.text
-  linkedList.tail.value.tail = <Circle
-  letter={text}
-  state={ElementStates.Changing}
-  isSmall={true}
-/>
-linkedList.tail.value.text = '';
-setChange(false)
-setBtnDellHead(false)
-}
-  const deleteTail = async() => {
+  const animDeleteTail = async () => {
+    setChange(true)
+    let text = linkedList.tail.value.text
+    linkedList.tail.value.tail = <Circle
+      letter={text}
+      state={ElementStates.Changing}
+      isSmall={true}
+    />
+    linkedList.tail.value.text = '';
+    setChange(false)
+    setBtnDellHead(false)
+  }
+  const deleteTail = async () => {
     setbtnDellTail(true)
     setStarted(true)
     animDeleteTail()
     await new Promise<void>((res) => {
       setTimeout(() => {
-          res()
+        res()
       }, 1000)
-  });
+    });
     linkedList.deleteTail();
     linkedList.tail.value.tail = 'tail'
     setListArray(linkedList.toArray())
@@ -198,7 +206,7 @@ setBtnDellHead(false)
     setbtnDellTail(false)
   }
 
-  const addByIndex = async() => {
+  const addByIndex = async () => {
     setBtnAddByIndex(true)
     setStarted(true)
     await linkedList.addByIndex({
@@ -207,33 +215,125 @@ setBtnDellHead(false)
       head: '',
       tail: ''
     }, indexInput, (
-    <Circle
-    letter={textInput}
-    state={ElementStates.Changing}
-    isSmall={true}
-  />))
+      <Circle
+        letter={textInput}
+        state={ElementStates.Changing}
+        isSmall={true}
+      />));
+    let i = 0;
+    while (i <= Number(indexInput)) {
+      const copy = [...listArray];
+      if (i != 0) {
+        copy[i - 1].value.head = ''
+        copy[0].value.head = 'head';
+        copy[i].value.head = <Circle
+          letter={textInput}
+          state={ElementStates.Changing}
+          isSmall={true} />
+        copy[i - 1].value.style = ElementStates.Changing
+      }
+      else {
+        copy[i].value.head = <Circle
+          letter={textInput}
+          state={ElementStates.Changing}
+          isSmall={true}
+        />;
+      }
+      await new Promise<void>((res) => {
+        setTimeout(() => {
+          res()
+        }, 1000)
+      });
+      setListArray(copy)
+      i++
+    }
+    if (i > indexInput) {
+      const copy = [...listArray];
+      copy[i - 1].value.head = '';
+      copy.forEach((el) => {
+        el.value.style = ElementStates.Default
+      })
+      await new Promise<void>((res) => {
+        setTimeout(() => {
+          res()
+        }, 1000)
+      });
+      await setListArray(linkedList.toArray());
+      await new Promise<void>((res) => {
+        setTimeout(() => {
+          res()
+        }, 1000)
+      });
+      const copy2 = [...linkedList.toArray()]
+      copy2.forEach((el) => {
+        el.value.style = ElementStates.Default;
+        el.value.head = '';
+      })
+      copy2[0].value.head = 'head';
+      copy2[copy2.length - 1].value.tail = 'tail';
+      setListArray(copy2)
+    }
     setStarted(false)
-    setListArray(linkedList.toArray());
     setBtnAddByIndex(false)
   }
-  const cicleCallback = (data:string | number) => {
-    return (
-      <Circle
-      letter={data}
-      state={ElementStates.Changing}
-      isSmall={true}
-    />)
-  }
-  const deleteByIndex  = async() => {
-    setStarted(true)
-    setDellAddByIndex(true)
-    await linkedList.deleteByIndex(indexInput, cicleCallback);
-    setListArray(linkedList.toArray())
+  const deleteByIndex = async () => {
+    if(indexInput < 0  || indexInput > listArray.length - 1) {
+      alert('Ошибка индекса')
+    }
+    else {
+      setStarted(true)
+      setDellAddByIndex(true);
+    let i = 0;
+    while (i <= Number(indexInput)) {
+      const copy = [...listArray];
+      copy[i].value.style = ElementStates.Changing
+      await new Promise<void>((res) => {
+        setTimeout(() => {
+          res()
+        }, 500)
+      });
+      setListArray(copy)
+      if(i == Number(indexInput)) {
+        await new Promise<void>((res) => {
+          setTimeout(() => {
+            res()
+          }, 500)
+        });
+        let text = copy[i].value.text
+        copy[i].value.style = ElementStates.Default;
+        copy[i].value.text = '';
+        copy[i].value.tail = <Circle
+        letter={text}
+        state={ElementStates.Changing}
+        isSmall={true}
+      />
+      setChange(true)
+      setListArray(copy)
+      }
+      i++
+    }
+    if(i>Number(indexInput)) {
+      await new Promise<void>((res) => {
+        setTimeout(() => {
+          res()
+        }, 500)
+      });
+      await linkedList.deleteByIndex(indexInput);
+      const copy = [...linkedList.toArray()]
+      if(copy.length !== 0) {
+        copy.forEach((el) => {
+          el.value.style = ElementStates.Default;
+        })
+        copy[0].value.head = 'head';
+        copy[copy.length - 1].value.tail = 'tail';
+      }
+      setListArray(copy)
+      setChange(false)
+    }
     setStarted(false)
     setDellAddByIndex(false)
   }
-
-
+  }
   return (
     <SolutionLayout title="Связный список">
       <div className={listStyle.box_main}>
@@ -243,17 +343,17 @@ setBtnDellHead(false)
             value={textInput}
             onChange={onFormTextChange}
             extraClass={listStyle.input_field_size}
-            />
+          />
           <Button
-            disabled={started}
-            isLoader = {btnAddHead}
+            disabled={started || textInput == '' }
+            isLoader={btnAddHead}
             text='Добавить в head'
             linkedList='big'
             extraClass={listStyle.button_size}
             onClick={addElHead}
           />
           <Button
-            disabled={started}
+            disabled={started || textInput == '' }
             isLoader={btnAddTail}
             text='Добавить в tail'
             linkedList='big'
@@ -261,15 +361,15 @@ setBtnDellHead(false)
             onClick={addElTail}
           />
           <Button
-            disabled={started}
+            disabled={started || listArray.length == 1}
             isLoader={btnDellHead}
             text='Удалить из head'
             linkedList='big'
             extraClass={listStyle.button_size}
             onClick={deleteHead}
           />
-           <Button
-            disabled={started}
+          <Button
+            disabled={started || listArray.length == 1}
             isLoader={btnDellTail}
             text='Удалить из tail'
             linkedList='big'
@@ -278,23 +378,23 @@ setBtnDellHead(false)
           />
         </div>
         <div className={`${listStyle.box_input} mt-6 mb-40`}>
-          <Input 
+          <Input
             type="number"
-           isLimitText={true}
+            isLimitText={true}
             maxLength={4}
             value={indexInput}
             onChange={onFormindexChange}
             extraClass={listStyle.input_field_size}
-            />
+          />
           <Button
-            disabled={started}
+            disabled={started || !indexInput || textInput == ''}
             isLoader={btnAddByIndex}
             text='Добавить  по индексу'
             onClick={addByIndex}
             linkedList='big'
           />
           <Button
-            disabled={started}
+            disabled={started || indexInput > listArray.length - 1 || listArray.length == 1}
             isLoader={btnDellByIndex}
             text='Удалить по индексу'
             linkedList='big'
@@ -312,7 +412,7 @@ setBtnDellHead(false)
                   state={list.value.style}
                   tail={list.value.tail}
                 />
-                {listArray.length - 1 != index && (<ArrowIcon/>)}
+                {listArray.length - 1 != index && (<ArrowIcon />)}
               </li>)
           }))}
         </ul>
